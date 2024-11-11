45a9bde5c97156f8f23f8b84837b3375946c5d8b
Steffi Eaint <t3moea00@students.oamk.fi>
2024-11-11 12:01:57 +0200

    added backend for search movies

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,52 @@
 import express from 'express';
 import cors from 'cors';
+import axios from 'axios';
 
 const port = 3001;
+const tmdb_api_key = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYjQ4NjkxNDc2NTEyMzk3NmVkZmQyMDBhMDNkNTE4ZiIsIm5iZiI6MTczMTMxNzkyNS41ODU3ODI4LCJzdWIiOiI2NzMxYmQ5ZjYxNjI2YWMxMDZiZTY3ZTQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.1M1h2M2yeqOEiy5JM46dtKHEDRj_a7y5LLd5308GIbk"
 
 const app = express();
 app.use(cors());
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.json({ message: 'Hello from server!' });
+})
+
+app.get('/search-movies', async(req,res) => {
+    const { query } = req.query;
+
+    if(!query) {
+        return res.status(400).json({ error: 'Query is required' });
+    }
+
+    try {
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${tmdb_api_key}&query=${encodeURIComponent(query)}`;
+
+        const response = await axios.get(url, {
+            headers: {
+                Authorization: `Bearer ${tmdb_api_key}`
+            }
+        });
+
+        const movies = response.data.results;
+
+        if(!movies || movies.length === 0) {
+            return res.status(404).json({ error: 'Movie not found' });
+        }
+
+        const formattedMovies = movies.map((movie) => {
+            return {
+                id: movie.id,
+                title: movie.title,
+                overview: movie.overview,
+                poster_path: movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : null,
+                release_date: movie.release_date
+            }
+        })
+        res.json(formattedMovies);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch movie data' });
+    }
 })
 
 app.listen(port);
