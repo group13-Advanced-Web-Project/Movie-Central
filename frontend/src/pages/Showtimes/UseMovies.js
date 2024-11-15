3c82ef034d6e83acd020c0b5592013ea65a9a638
maheshidangodage <t3idma00@students.oamk.fi>
2024-11-15 12:01:33 +0200

    Added Showtimes.js, fetchShowTimes.js, UseMovie.js, Showtimes.css and edited App.js

---
+++ frontend/src/pages/Showtimes/UseMovies.js
@@ -0,0 +1,21 @@
+// src/pages/UseMovies.js
+import { useState, useEffect } from 'react';
+import { fetchShowSchedule } from './fetchShowSchedule'; 
+
+const useMovies = () => {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async (date, areaCode = '') => {
+    const shows = await fetchShowSchedule(date, areaCode);
+    setMovies(shows);
+  };
+
+  useEffect(() => {
+    const today = new Date().toISOString().split('T')[0];
+    fetchMovies(today);
+  }, []);
+
+  return { movies, fetchMovies };
+};
+
+export default useMovies;
