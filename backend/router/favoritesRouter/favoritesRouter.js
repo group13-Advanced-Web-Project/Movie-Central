773e656fff076fdc1622d7bf7be2dc784fcdbb3c
Sanjaya Bhattarai <t3bhsa00@students.oamk.fi>
2024-11-21 20:57:45 +0200

    added delete endpoint for favorite movies

--- backend/router/favoritesRouter/favoritesRouter.js
+++ backend/router/favoritesRouter/favoritesRouter.js
@@ -34,7 +34,7 @@ router.put('/update/:id', async (req, res) => {
     try {
         const { id } = req.params;
         const { fave_1, fave_2, fave_3, fave_4 } = req.body;
-        const parsedId = parseInt(id, 10);
+        const parsedId = parseInt(user_id, 10);
 
         if (isNaN(parsedId)) {
             return res.status(400).json({
@@ -66,7 +66,64 @@ router.put('/update/:id', async (req, res) => {
     }
 });
 
+// Delete favorite
 
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { fave_1, fave_2, fave_3, fave_4 } = req.body;
 
+        let query = "UPDATE favorites SET";
+        let updates = [];
+        let params = [];
+        let index = 1;
+
+        if (fave_1) {
+            updates.push(`fave_1 = NULL`);
+        }
+        if (fave_2) {
+            updates.push(`fave_2 = NULL`);
+        }
+        if (fave_3) {
+            updates.push(`fave_3 = NULL`);
+        }
+        if (fave_4) {
+            updates.push(`fave_4 = NULL`);
+        }
+
+        if (updates.length === 0) {
+            return res.status(400).json({
+                error: "No fields to update",
+                details: "No favorite fields were provided to delete",
+            });
+        }
+
+        query += ` ${updates.join(', ')} WHERE id = $${index}`;
+        params.push(id);
+
+        pool.query(query, params, (error, results) => {
+            if (error) {
+                console.error("Database query failed:", error.message);
+                return res.status(500).json({
+                    error: "Database query failed",
+                    details: error.message,
+                });
+            }
+            if (results.rowCount === 0) {
+                return res.status(404).json({
+                    error: "Favorite entry not found",
+                    details: `No favorite entry found with id ${id}`,
+                });
+            }
+            res.status(200).json({ message: "Favorite movies deleted successfully" });
+        });
+    } catch (error) {
+        console.error("Database query failed:", error.message);
+        return res.status(500).json({
+            error: "Database query failed",
+            details: error.message,
+        });
+    }
+});
 
 export default router;
