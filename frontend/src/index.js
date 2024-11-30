05562c9f447b68975e842b8d862abadd85818d59
fuzzyduckers <83663235+fuzzyduckers@users.noreply.github.com>
2024-11-30 10:22:25 +0200

    fix: user stays logged in on page reload

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { createRoot } from 'react-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
-import { MoviesProvider } from './context/MoviesContext';
-import './index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { MoviesProvider } from "./context/MoviesContext";
+import "./index.css";
+import App from "./App";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
 root.render(
   <Auth0Provider
     domain="dev-ceeovj0kndp8rnbf.us.auth0.com"
     clientId="Qc6QVZmgimeV13Y8adloRn9siwrSh3AG"
     authorizationParams={{
-      redirect_uri: window.location.origin
+      redirect_uri: window.location.origin,
     }}
+    redirect_uri={window.location.origin}
+    useRefreshTokens
+    cacheLocation="localstorage"
   >
     <MoviesProvider>
       <App />
