25e62a9067306f0b224056c6aa566c9d4020090a
fuzzyduckers <83663235+fuzzyduckers@users.noreply.github.com>
2024-11-30 12:06:59 +0200

    chore: update createRoot import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client"; 
 import { Auth0Provider } from "@auth0/auth0-react";
 import { MoviesProvider } from "./context/MoviesContext";
 import "./index.css";
