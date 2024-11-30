abc9ebc16824af7586f85138eeb7233739fc51fe
fuzzyduckers <83663235+fuzzyduckers@users.noreply.github.com>
2024-11-30 11:49:24 +0200

    fix: admin page, group_members table is_admin shows appropriate to boolean value

--- frontend/src/components/admin/GroupMembersTable.js
+++ frontend/src/components/admin/GroupMembersTable.js
@@ -4,58 +4,54 @@ import { useEffect } from "react";
 const serverUrl = process.env.REACT_APP_API_URL;
 
 function GroupMembersTable() {
-  const [groupMembersData, setGroupMembersData] = useState([]);
+    const [groupMembersData, setGroupMembersData] = useState([]);
 
-  const getGroupsTable = async () => {
-    const response = await fetch(serverUrl + "/admin/Group_members", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("auth0:id_token")}`,
-      },
-    });
-    setGroupMembersData(await response.json());
+    const getGroupsTable = async () => {
+        const response = await fetch(serverUrl + "/admin/Group_members", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("auth0:id_token")}`,
+            },
+        });
+        setGroupMembersData(await response.json());
+    };
 
-    // console.log(response);
-    // console.log(groupsData);
-  };
+    useEffect(() => {
+        getGroupsTable();
+    }, []);
 
-  useEffect(() => {
-    getGroupsTable();
-  }, []);
-
-  return (
-    <>
-      <div>
-        <h2>Groups_Members Table</h2>
-      </div>
-      <div className="table-container">
-        <table className="tables-table">
-          <thead>
-            <tr>
-              <th>id</th>
-              <th>group_id</th>
-              <th>group_id</th>
-              <th>is_admin</th>
-              <th>status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {groupMembersData.map((user, index) => (
-              <tr key={index}>
-                <td>{user.id}</td>
-
-                <td>{user.group_id}</td>
-                <td>{user.user_id}</td>
-                <td>{user.is_admin}</td>
-                <td>{user.status}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <h2>Groups_Members Table</h2>
+            </div>
+            <div className="table-container">
+                <table className="tables-table">
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>group_id</th>
+                            <th>user_id</th>
+                            <th>is_admin</th>
+                            <th>status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {groupMembersData.map((user, index) => (
+                            <tr key={index}>
+                                <td>{user.id}</td>
+                                <td>{user.group_id}</td>
+                                <td>{user.user_id}</td>
+                                <td>{user.is_admin ? "Yes" : "No"}</td>
+                                <td>{user.status}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
 }
 
 export default GroupMembersTable;
