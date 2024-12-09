6807bfa0e5cc82ef8c9b4dcdf381fb5c15747973
fuzzyduckers <83663235+fuzzyduckers@users.noreply.github.com>
2024-12-09 14:59:49 +0200

    fault finding, deploy with no env - 1

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,9 +18,9 @@ root.render(
   <Auth0Provider
     domain={domain}
     clientId={clientId}
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}
+    // authorizationParams={{
+    //   redirect_uri: window.location.origin,
+    // }}
     useRefreshTokens
     cacheLocation="localstorage"
   >
