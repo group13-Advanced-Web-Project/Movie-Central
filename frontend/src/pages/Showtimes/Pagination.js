287d48655c1a7d6dd15d07fdde560a610f145c44
maheshidangodage <t3idma00@students.oamk.fi>
2024-12-11 09:49:35 +0200

    Fixed the issue with pagination responsiveness on the Showtime page.

--- frontend/src/pages/Showtimes/Pagination.js
+++ frontend/src/pages/Showtimes/Pagination.js
@@ -1,32 +1,40 @@
 import React from 'react';
 import '../../styles/Showtimes.css';
 
-const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+const Pagination = ({ currentPage, totalPages, goToPreviousPage, goToPage, goToNextPage }) => {
   return (
-    <div className="pagination-controls">
-      <button
-        className="pagination-button"
-        onClick={() => onPageChange(currentPage - 1)}
-        disabled={currentPage === 1}
-      >
-        Previous
-      </button>
-      {Array.from({ length: totalPages }, (_, index) => index + 1).map((page) => (
+    <div className="pagination-container">
+      <div className="pagination-row upper-row">
         <button
-          key={page}
-          className={`pagination-number ${currentPage === page ? 'active' : ''}`}
-          onClick={() => onPageChange(page)}
+          className="pagination-button"
+        disabled={currentPage === 1}
+          onClick={goToPreviousPage}
         >
-          {page}
+          Previous
         </button>
-      ))}
-      <button
-        className="pagination-button"
-        onClick={() => onPageChange(currentPage + 1)}
-        disabled={currentPage === totalPages}
-      >
-        Next
-      </button>
+      </div>
+
+      <div className="pagination-row middle-row">
+        {[...Array(totalPages).keys()].map((page) => (
+          <button
+            key={page + 1}
+            className={`pagination-number ${currentPage === page + 1 ? 'active' : ''}`}
+            onClick={() => goToPage(page + 1)}
+          >
+            {page + 1}
+          </button>
+        ))}
+      </div>
+
+      <div className="pagination-row lower-row">
+        <button
+          className="pagination-button"
+          disabled={currentPage === totalPages}
+          onClick={goToNextPage}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
